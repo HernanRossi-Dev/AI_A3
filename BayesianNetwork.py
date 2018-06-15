423597b54cd7f160974d5a75dd5dc926b5aa4ec0
HernanRossi <hernan_rossi@msn.com>
2018-06-14 17:01:20 -0700

    bayesian network

---
+++ BayesianNetwork.py
@@ -0,0 +1,58 @@
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+from pgmpy.inference import BeliefPropagation
+
+class BayesianNetwork:
+    def __init__(self):
+        self.musicModel = BayesianModel([('Difficulty', 'Rating'), ('Musicianship', 'Rating'),
+                                         ('Rating', 'Letter'), ('Musicianship', 'Exam')])
+
+    def start(self):
+
+        cpd_difficulty = TabularCPD(variable='Difficulty', variable_card=2,
+                                    values=[[0.6], [0.4]])
+        cpd_musicianship = TabularCPD(variable='Musicianship', variable_card=2,
+                                    values=[[0.7], [0.3]])
+
+        cpd_Rating = TabularCPD(variable='Rating', variable_card=3,
+                                    values=[[0.3, 0.05, 0.9, 0.5],
+                                            [0.4, 0.25, 0.08, 0.3],
+                                            [0.3, 0.7, 0.02, 0.2]],
+                                            evidence=['Difficulty', 'Musicianship'],
+                                            evidence_card=[2, 2])
+
+        cpd_Exam = TabularCPD(variable='Exam', variable_card=2,
+                                values=[[0.95, 0.2],
+                                        [0.05, 0.8]],
+                                evidence=['Musicianship'],
+                                evidence_card=[2])
+
+        cpd_Letter = TabularCPD(variable='Letter', variable_card=2,
+                                values=[[0.1, 0.4, 0.99],
+                                        [0.9, 0.6, 0.01]],
+                                evidence=['Rating'],
+                                evidence_card=[3])
+        self.musicModel.add_cpds(cpd_difficulty, cpd_musicianship, cpd_Rating, cpd_Exam, cpd_Letter)
+        print(self.musicModel.check_model())
+        infer = VariableElimination(self.musicModel)
+        examResult = infer.query(
+            variables=['Exam'], evidence={'Musicianship':1})['Exam']
+        musicResult = infer.query(
+            variables=['Musicianship'])['Musicianship']
+        ratingResult = infer.query(
+            variables=['Rating'], evidence={'Musicianship':1, 'Difficulty':0})['Rating']
+        diffResult = infer.query(
+            variables=['Difficulty'])['Difficulty']
+        letterResult = infer.query(
+            variables=['Letter'], evidence={'Rating':1})
+
+        print(examResult)
+        print(musicResult)
+        print(ratingResult)
+        print(diffResult)
+
+        print(letterResult.values())
+
+bayesianNetwork = BayesianNetwork()
+bayesianNetwork.start()
